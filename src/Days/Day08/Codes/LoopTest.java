d69ed6dbe8225a6ef17d1d867254a8b10649d806
WestbrookYuan <yuan308389197@icloud.com>
2022-12-22 21:35:14 -0600

    break continue

--- src/Days/Day08/Codes/LoopTest.java
+++ src/Days/Day08/Codes/LoopTest.java
@@ -13,31 +13,49 @@ public class LoopTest {
 //            System.out.println(temp);
 //        }
 
-        for (int i=0; i < 5; i++){
-            for(int j=0; j< 5; j++){
-                System.out.println(i*j);
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.println(i * j);
             }
         }
 
-        for (int i=0; i < 5;i++){
-            for(int j=0;j<i+1;j++){
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i + 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
 
-        for(int i = 1;i <= 100;i++){
-            if (isPrime(i)){
+        for (int i = 1; i <= 100; i++) {
+            if (isPrime(i)) {
                 System.out.println(i);
             }
         }
 
+        for (int i = 0; i < 11; i++) {
+            if (i % 4 == 0) {
+                continue; //123567910
+            }
+            System.out.print(i);
+        }
+        System.out.println();
+
+        label:for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++){
+                if (j % 4 == 0) {
+                    break label; //123567910
+                }
+                System.out.print(j);
+            }
+            System.out.println();
+
+        }
     }
 
-    public static boolean isPrime(int num){
+    public static boolean isPrime(int num) {
         boolean isPrime = true;
-        for(int j = 2;j < num; j++){
-            if(num % j == 0){
+        for (int j = 2; j < num / 2; j++) {
+            if (num % j == 0) {
                 isPrime = false;
                 break;
             }
--- src/Days/Day08/Mdfiles/Loop.md
+++ src/Days/Day08/Mdfiles/Loop.md
@@ -7,6 +7,17 @@ Loop
 * While(true)
   * consider the condition inside the loop to break the loop
 * Loop in Loop:
+* break, continue
+  * break: 
+    * used in loop and switch-case
+    * shut down the current loop
+    * shut down the closet loop
+  * continue:
+    * skip the loop(one time)
+    * skip the closet loop
+
+* break continue with tag:
+  * shut down the loop where has a tag
 * [Code Example](../Codes/LoopTest.java)
 
 [Back to Director](https://github.com/WestbrookYuan/Java-Learning/)
